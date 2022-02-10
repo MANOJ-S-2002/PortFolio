d0489d200b7cf8fecc53796c995ac8e0b24572a5
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-02-10 11:42:40 +0530

    resolved warnings

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -14,6 +14,7 @@ function About() {
           <img
             src={require("../../../assets/image/coding.png")}
             className="picture"
+            alt="coding"
           />
         </div>
       </div>
--- src/data/work.js
+++ src/data/work.js
@@ -6,7 +6,7 @@ export const WorkData = [
     dateEnd: "Present",
     companyLogo:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png",
-    work: "Nothing",
+    work: "I have worked with different people from different area",
   },
   {
     company: "Facebook",
@@ -15,6 +15,6 @@ export const WorkData = [
     dateEnd: "Present",
     companyLogo:
       "https://iconape.com/wp-content/png_logo_vector/facebook-f-logo-2019.png",
-    work: "Nothing",
+    work: "I have worked with different people from different area",
   },
 ];
