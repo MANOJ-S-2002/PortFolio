0b145fb8351567d19f61d4597a290d4e1244c77f
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-07-15 21:42:18 +0530

    updated

--- src/data/work.js
+++ src/data/work.js
@@ -1,20 +1,20 @@
 export const WorkData = [
   {
-    company: "Google",
+    company: "SMARTBRIDGE",
     designation: "Software Engineer",
     dateJoining: "02-02-2018",
     dateEnd: "Present",
     companyLogo:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png",
+      "",
     work: "I have worked with different people from different area",
   },
   {
-    company: "Facebook",
+    company: "ICONIC DREAM FOCUS",
     designation: "Software Engineer",
-    dateJoining: "02-02-20203",
+    dateJoining: "02-02-2023",
     dateEnd: "Present",
     companyLogo:
-      "https://iconape.com/wp-content/png_logo_vector/facebook-f-logo-2019.png",
+      "",
     work: "I have worked with different people from different area",
   },
 ];
