bc8b91839d0d0583bce67a7eb277f9dff27ffe9a
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-07-15 22:06:13 +0530

    updated

--- src/data/work.js
+++ src/data/work.js
@@ -1,20 +1,19 @@
 export const WorkData = [
   {
     company: "SMARTBRIDGE",
-    designation: "Software Engineer",
-    dateJoining: "02-02-2018",
-    dateEnd: "Present",
-    companyLogo:
-      "",
+    designation: "Salesforce Developer",
+    dateJoining: "25-04-2022",
+    dateEnd: "25-06-2022",
+    companyLogo:require("../assets/image/company1.png"),
     work: "I have worked with different people from different area",
   },
   {
     company: "ICONIC DREAM FOCUS",
-    designation: "Software Engineer",
-    dateJoining: "02-02-2023",
+    designation: "React Native Developer",
+    dateJoining: "15-07-2022",
     dateEnd: "Present",
-    companyLogo:
-      "",
+    companyLogo:require("../assets/image/company2.png"),
+
     work: "I have worked with different people from different area",
   },
 ];
